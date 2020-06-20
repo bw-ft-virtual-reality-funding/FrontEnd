0a9178f598bdb84d88e91798739e0c6361040525
meep-morp <gracieaerogirl@gmail.com>
2020-06-20 13:03:47 -0500

    Added less and css files & folders

--- vr-funding/src/components/App.js
+++ vr-funding/src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import "./styles/App.css";
+import logo from "./styles/images/VRFP_logo.png";
+import "./styles/less/index.less";
 
 function App() {
 	return <div className="App">
     <Router>
+
+     {/* HOME PAGE -- SIGNUP & LOGIN  */}
       <Route path="/" exact>
+          <img src={logo} alt=""/>
+      </Route>
+
+      { /*  DASHBOARD  */ }
+      <Route >
 
       </Route>
     </Router>
