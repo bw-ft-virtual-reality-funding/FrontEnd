00a836baed5066786d22b7e94961879c9820566b
Cristina Altreche <c.altreche@gmail.com>
2020-06-22 13:06:29 -0500

    Login.js initiated

--- vr-funding/src/components/App.js
+++ vr-funding/src/components/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Nav from "./nav";
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./styles/css/index.css";
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
-	return <div className="App">
-    <Router>
+  return (
+    <div className="App">
+      <Router>
+        {/* HOME PAGE -- SIGNUP & LOGIN  */}
+        <Route path="/" exact>
+          <Nav>
+            <Link to="/Login">Login</Link>
+          </Nav>
+        </Route>
 
-     {/* HOME PAGE -- SIGNUP & LOGIN  */}
-      <Route path="/" exact>
-        <Nav />
-      </Route>
-
-      { /*  DASHBOARD  */ }
-      <PrivateRoute path="/dashboard">
-
-      </PrivateRoute>
-    </Router>
-  </div>;
+        {/*  DASHBOARD  */}
+        <PrivateRoute path="/dashboard"></PrivateRoute>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
