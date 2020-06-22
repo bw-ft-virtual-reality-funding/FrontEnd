61aaf618360d374468c9072450d13a9e1fbfb47e
Cristina Altreche <c.altreche@gmail.com>
2020-06-22 16:52:50 -0500

    Created validation folder and FormSchema's for both SignUp and Login

--- vr-funding/src/components/login-signup/Login.js
+++ vr-funding/src/components/login-signup/Login.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from 'react-router-dom'
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import * as yup from 'yup'
+import loginFormSchema from './validation/loginFormSchema'
+import axios from 'axios'
 
 const initialVal = {
     username: '',
@@ -17,23 +20,65 @@ const initialUser = [];
 
 export default function Login() {
 const [disabled, setDisabled] = useState(true);
-    const onSubmitHandler = e => {
+const [user, setUser] = useState(initialUser);
+const [formValues, setFormValues] = useState(initialVal)
+const [formErrors, setFormErrors] = useState(initialErr)
+    
+
+
+const onSubmitHandler = e => {
         axiosWithAuth()
         .post(`URL`, )
     }
 
+const postUser = (newUser) => {
+    axios.post("https://regres.in/api/users", newUser)
+    .then((response) => {
+        setUser([...user, response.data]);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .finally(() => {
+        setFormValues(initialVal)
+    })
+}
+
+const onInputChange = (e) => {
+    const { name, value } = e.target;
+    
+    yup.reach(loginFormSchema, name)
+        .validate(value)
+        .then((valid) => {
+            setFormErrors({
+                ...formErrors,
+                [name]: "",
+            })
+        })
+        .catch((err) => {
+            setFormErrors({
+                ...formErrors,
+                [name]: err.errors[0],
+            })
+        })
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        })
+}
+
   return <div>
-      <form>
+      <form >
           <h1>Login</h1>
           
           <label htmlFor="username">
               Username&nbsp;
-              <input name="username" type="text"/>
+              <input name="username" type="text" />
           </label>
 
           <label htmlFor="password">
               Password&nbsp;
-              <input name="password" type="password"/>
+              <input name="password" type="password" />
           </label>
 
           <div>
--- vr-funding/src/components/login-signup/Signup.js
+++ vr-funding/src/components/login-signup/Signup.js
@@ -1,49 +1,100 @@
 import React, { useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import signUpFormSchema from './validation/signUpFormSchema'
+import * as yup from "yup";
+import axios from "axios";
+
+const initialVal = {
+  firstname: "",
+  lastname: "",
+  username: "",
+  email: "",
+  password: "",
+};
+
+const initialErr = {
+  firstname: "",
+  lastname: "",
+  username: "",
+  email: "",
+  password: "",
+};
+
+const newUser = [];
 
 export default function Signup() {
-    const [disabled, setDisabled] = useState(true);
+  const [disabled, setDisabled] = useState(true);
+  const [user, setUser] = useState(newUser);
+  const [formValues, setFormValues] = useState(initialVal);
+  const [formErrors, setFormErrors] = useState(initialErr);
 
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
 
-    return <div>
-    <form>
+    yup
+      .reach(signUpFormSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <form>
         <h1>Sign Up</h1>
-        <h3>Create you account.</h3>
-        
+        <h3>Create your account.</h3>
+
         <label htmlFor="firstname">
-            First Name&nbsp;
-            <input name="firstname" type="text"/>
+          First Name&nbsp;
+          <input name="firstname" type="text" />
         </label>
         <label htmlFor="lastname">
-            Last Name&nbsp;
-            <input name="lastname" type="text"/>
+          Last Name&nbsp;
+          <input name="lastname" type="text" />
         </label>
 
         <label htmlFor="email">
-            Email&nbsp;
-            <input name="email" type="email"/>
+          Email&nbsp;
+          <input name="email" type="email" />
+        </label>
+
+        <label htmlFor="username">
+          Email&nbsp;
+          <input name="username" type="text" />
         </label>
 
         <label htmlFor="password">
-            Password&nbsp;
-            <input name="password" type="password"/>
-        </label>
-
-        <label htmlFor="confirm_password">
-            Confirm Password&nbsp;
-            <input name="confirm_password" type="password"/>
+          Password&nbsp;
+          <input name="password" type="password" />
         </label>
 
         <div>
-            <button disabled={disabled}>Sign Up Now</button>
+          <button disabled={disabled}>Sign Up Now</button>
         </div>
 
         <div className="errors">
-            <div></div>
-            <div></div>
+          <div></div>
+          <div></div>
         </div>
-    </form>
+      </form>
 
-    <div>Already have an account?<Link to="/">Login</Link> </div>
-</div>;
+      <div>
+        Already have an account?<Link to="/">Login</Link>{" "}
+      </div>
+    </div>
+  );
 }
