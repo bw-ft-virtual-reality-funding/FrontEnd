9d02e5305ecaf61aa4228a577dc8702bc665506e
meep-morp <gracieaerogirl@gmail.com>
2020-06-24 13:13:17 -0500

    Added the ability to edit projects

--- vr-funding/src/components/dashboard/funder/fundPage.jsx
+++ vr-funding/src/components/dashboard/funder/fundPage.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react'
 import FundraiserCard from "../fundraiser/FundraiserCard";
 import { useParams } from "react-router-dom";
 import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import FundraiserForm from '../fundraiser/FundraiserForm';
 
 const FundPage = props => {
     const [project, setProject] = useState({});
+    const [isEditing, setIsEditing] = useState(false);
 
     const UrlId = useParams();
     const id = UrlId.id;
@@ -23,10 +25,32 @@ const FundPage = props => {
 
     return (
         <div className="full">
+            {
+                isEditing
+                    ?
+                    <div className="modal openModal">
+                        <div className="cancelX" onClick={(e) => {
+                            e.preventDefault();
+                            setIsEditing(false)
+                        }}>X</div>
+                        <FundraiserForm
+                            URL={`https://virtual-reality-fundraising.herokuapp.com/api/projects/${id}`}
+                            values={project}
+                            put="true"
+                            setIsEditing={setIsEditing}
+                            id={id}
+                        />
+                    </div>
+                    : ""
+            }
+
             <div className="projects">
                 <FundraiserCard details={project} />
             </div>
-            <button className="button">Pledge to Campaign</button>
+            <div className="buttons">
+                <button className="button pledge">Pledge to Campaign</button>
+                <button className="button" onClick={() => setIsEditing(true)}>Edit</button>
+            </div>
         </div>
     )
 }
