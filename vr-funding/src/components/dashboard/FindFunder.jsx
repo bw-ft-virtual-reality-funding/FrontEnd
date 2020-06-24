c8bee68c8e15f46fdd8f78e18a55f85c96673b21
Cristina Altreche <c.altreche@gmail.com>
2020-06-24 13:42:26 -0500

    Capital letter names displaying on Find Funder

--- vr-funding/src/components/dashboard/FindFunder.jsx
+++ vr-funding/src/components/dashboard/FindFunder.jsx
@@ -1,55 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import { axiosWithAuth } from '../utils/axiosWithAuth'
-import gsap from 'gsap'
-import { Button, Avatar } from '@material-ui/core'
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import gsap from "gsap";
+import { Button, Avatar } from "@material-ui/core";
 
-const initialFunder = []
+const initialFunder = [];
 
-export default function FindFunder () {
-  const [funders, setFunders] = useState(initialFunder)
+export default function FindFunder() {
+  const [funders, setFunders] = useState(initialFunder);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("https://virtual-reality-fundraising.herokuapp.com/api/users")
+
+      .then((response) => {
+        // console.log(response.data);
+        setFunders(response.data);
+      })
+
+      .catch((err) => {
+        console.log(err, "This did not work");
+      });
+  }, []);
 
   const capital = (str) => {
-        const firstLetter = str.substring(0, 1);
+    const firstLetter = str.substring(0, 1);
 
-        if (firstLetter == firstLetter.toUpperCase()) {
-            return true
-        } else return false
-  }
-  
-
-  useEffect (() => {
-
-      axiosWithAuth().get('https://virtual-reality-fundraising.herokuapp.com/api/users')
-
-      .then(response =>{
-          console.log(response.data)
-          setFunders(response.data)
-      })
-
-      .catch(err =>{
-          console.log(err, 'This did not work')
-      })
-  }, [])
-
-  console.log(funders)
-
-funders.filter(data => {
-    capital(data.name);
-})
+    if (firstLetter == firstLetter.toUpperCase()) {
+      return true;
+    } else return false;
+  };
  
-    return (
-        <div className="full">
-            <h2>Find a User</h2>
-            <p>Here is a list of our current users</p>
-       {funders.map((funder) =>{
-           return (
-               <>
-            <div>{funder.name} -- {funder.role}</div>
-            {/* <Button color="primary">Im a button</Button>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
-            </>
-           )
-       })}
-      </div>
-    )
+
+  return (
+    <div className="full">
+      <h2>Find a User</h2>
+      <p>Here is a list of our current users</p>
+
+     {funders.map((data) => {
+         capital(data.name)
+         if(capital(data.name) === true) {
+             return (
+                 <div>{data.name} -- {data.role}</div>
+                     
+             )
+         }
+     })}
+    </div>
+  );
 }
