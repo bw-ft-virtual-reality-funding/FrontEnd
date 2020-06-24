ca84c05cae3e6c855a94f23e601cd3081284ce7e
meep-morp <gracieaerogirl@gmail.com>
2020-06-24 15:15:44 -0500

    Added card styles to user

--- vr-funding/src/components/dashboard/FindFunder.jsx
+++ vr-funding/src/components/dashboard/FindFunder.jsx
@@ -4,52 +4,44 @@ import { axiosWithAuth } from '../utils/axiosWithAuth'
 //Will work on this more tomorrow 
 const initialFunder = []
 
-export default function FindFunder() {
-    const [funders, setFunders] = useState(initialFunder)
+export default function FindFunder () {
+  const [funders, setFunders] = useState(initialFunder)
 
-    const capital = (str) => {
+  const capital = (str) => {
         const firstLetter = str.substring(0, 1);
 
         if (firstLetter == firstLetter.toUpperCase()) {
             return true
         } else return false
-    }
+  }
+  
 
+  useEffect (() => {
 
-    useEffect(() => {
+      axiosWithAuth().get('https://virtual-reality-fundraising.herokuapp.com/api/users')
 
-        axiosWithAuth().get('https://virtual-reality-fundraising.herokuapp.com/api/users')
+      .then(response =>{
+          console.log(response.data)
+        //   setFunders(response.data)
+          setFunders(response.data.filter(data => capital(data.name)));
+      })
 
-            .then(response => {
-                console.log(response.data)
-                //   setFunders(response.data)
-                setFunders(response.data.filter(data => capital(data.name)));
-            })
+      .catch(err =>{
+          console.log(err, 'This did not work')
+      })
+  }, [])
 
-            .catch(err => {
-                console.log(err, 'This did not work')
-            })
-    }, [])
-
-    console.log(funders)
-
+  console.log(funders)
+ 
     return (
-        <>
-            <div className="full">
-                <div className="title">
-                    <h2>Find a User</h2>
-                </div>
-                <div className="cont">
-                {funders.map((funder) => {
-                    return (
-                        <div className="userCard">
-                            <h2>{funder.name}</h2>
-                            <p>{funder.role}</p>
-                        </div>
-                    )
-                })}
-                </div>
-            </div>
-        </>
+        <div className="full">
+            <h2>Find a User</h2>
+            <p>Here is a list of our current users</p>
+       {funders.map((funder) =>{
+           return (
+            <div>{funder.name} -- {funder.role}</div>
+           )
+       })}
+      </div>
     )
 }
