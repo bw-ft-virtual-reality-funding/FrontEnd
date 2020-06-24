9174dcda7b540149e2f37b4721e6963dc2d090bb
Cristina Altreche <c.altreche@gmail.com>
2020-06-24 16:51:40 -0500

    Badges added and cards flex

--- vr-funding/src/components/dashboard/FindFunder.jsx
+++ vr-funding/src/components/dashboard/FindFunder.jsx
@@ -4,14 +4,17 @@ import gsap from "gsap";
 import { Card } from "@material-ui/core";
 import FaceIcon from "@material-ui/icons/Face";
 import { makeStyles } from "@material-ui/core/styles";
+import funder from "../styles/images/funder.png"
+import fundraiser from "../styles/images/fundraiser.png"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
-    height: "300px",
+    height: "125px",
     width: "400px",
-    marginBottom: "10px",
+    margin: "10px",
     textAlign: "center",
+    padding: '40px 20px',
   },
 
   flex: {
@@ -36,6 +39,31 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "20px",
     paddingBottom: "30px",
   },
+
+  cards: {
+      maxWidth: '1000px',
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent:'center',
+      alignItems: 'center',
+  },
+
+  h3Style: {
+    fontSize: '18px',
+    paddingBottom: '10px',
+  },
+
+  role: {
+      textTransform: 'uppercase',
+      padding: '10px',
+  },
+
+  img: {
+      width: '50px',
+      marginTop: '5px',
+  }
+
+
 }));
 
 const initialFunder = [];
@@ -72,28 +100,56 @@ export default function FindFunder() {
   };
   console.log(funders);
 
- 
+ const badge = funders.map((data) => {
+     if(data.role === 'funder'){
+         return 'THIS'
+     } else if (data.role === 'fundraiser'){
+         return 'THAT'
+     }
+ })
 
   return (
     <div className="full">
+
       <div className={classes.flex}>
         <h2 className={classes.h2Style}>
           VRFP Users <FaceIcon className={classes.icon} />
         </h2>
         <p className={classes.pTag}>Here is a list of our current users</p>
       </div>
-      {funders.map((data) => {
+
+      <div className={classes.cards}> 
+      {funders.map((data) => 
+      {
         capital(data.name);
-        if (capital(data.name) === true) {
+        if (capital(data.name) === true && data.role === 'funder') 
+        {
           return (
             <Card  className={classes.root}>
-              <h3>{data.name}</h3>
-              <div>{data.role}</div>
-          {/* <icon>😁{}</icon> */}
+              <h3 className={classes.h3Style}>{data.name}</h3>
+              <div className={classes.role}>{data.role}</div>
+              <hr width="50%"/>
+              <div><img className={classes.img} src={funder}></img></div>
             </Card>
-          );
-        }
-      })}
+          )
+        } 
+        else if(capital(data.name) === true && data.role === 'fundraiser')
+        {
+            return (
+              <Card  className={classes.root}>
+                <h3 className={classes.h3Style}>{data.name}</h3>
+                <div className={classes.role}>{data.role}</div>
+                <hr width="50%"/>
+                <div><img className={classes.img} src={fundraiser}></img></div>
+              </Card>
+            )
+      }
+      
+    })}
+    
+
+      </div>
+
     </div>
   );
 }
