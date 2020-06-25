ed93a4703e47afc6594b6661c0d8111939f61417
meep-morp <gracieaerogirl@gmail.com>
2020-06-25 13:59:18 -0500

    Debugging deploy error

---
+++ vr-funding/build/precache-manifest.6fc438ce284bebb0536e36205ddde91a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cac76567e4ed0910a637265e4f8553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f0fb94040e323305b2",
+    "url": "/static/css/2.c73eaead.chunk.css"
+  },
+  {
+    "revision": "7c939430e2fb41d0f9c1",
+    "url": "/static/css/main.40b59221.chunk.css"
+  },
+  {
+    "revision": "00f0fb94040e323305b2",
+    "url": "/static/js/2.ce44f5a3.chunk.js"
+  },
+  {
+    "revision": "ccd3efce5fbc9204df1b0007b492a558",
+    "url": "/static/js/2.ce44f5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c939430e2fb41d0f9c1",
+    "url": "/static/js/main.2f112ecf.chunk.js"
+  },
+  {
+    "revision": "532dadb359f3bd1568d6",
+    "url": "/static/js/runtime-main.b3c8760f.js"
+  },
+  {
+    "revision": "39eb0bbf2cc33ba02f53f8585004f820",
+    "url": "/static/media/defaultPFP.39eb0bbf.jpg"
+  },
+  {
+    "revision": "adad84ef34495be7dfee018b984e2f29",
+    "url": "/static/media/funder.adad84ef.png"
+  },
+  {
+    "revision": "25bed55f07a49c41b55aac4a721afac9",
+    "url": "/static/media/fundraiser.25bed55f.png"
+  },
+  {
+    "revision": "013221d62ab20eee6eedb2172ced88e7",
+    "url": "/static/media/hide.013221d6.svg"
+  },
+  {
+    "revision": "6a9d678d39afb26b74bfaf58fb169c10",
+    "url": "/static/media/placeholder.6a9d678d.png"
+  },
+  {
+    "revision": "c3e1e28f3d86036d49930422ad501d29",
+    "url": "/static/media/shown.c3e1e28f.svg"
+  }
+]);
