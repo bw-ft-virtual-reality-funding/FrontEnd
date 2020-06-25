e8c369d06cac9a1cf978acbebbf0a8a4ae05ae24
Khalil <khalilfoulks@gmail.com>
2020-06-25 10:38:12 -0400

    moved the gsap animation to fundraisercard

--- vr-funding/src/components/dashboard/fundraiser/FundraiserCard.js
+++ vr-funding/src/components/dashboard/fundraiser/FundraiserCard.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React , { useEffect } from "react";
 import { Link } from "react-router-dom";
 import placeholder from "../../styles/images/placeholder.png";
+import { gsap } from 'gsap'
 
 export default function FundraiserCard(props) {
 	const { details } = props;
 
+	useEffect(() =>{
+		const container = (document.querySelectorAll(".projects .container"))	
+		console.log(container)
+		container.forEach(element => {
+			element.addEventListener('mouseover', function(event) {
+				gsap.fromTo('.projects .container', {opacity:0, y:-300}, {stagger:1, y:1, opacity:1, duration:1})
+			});
+		});
+	}, [])
+	
 	if (!details) {
 		return <h3>Working on fetching your fundraiser...</h3>;
 	}
